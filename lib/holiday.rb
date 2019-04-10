7f7581c59e0439e11515ae91c645c7d6b94a882f
Henry Koehler <henryk1229@gmail.com>
2019-04-10 17:03:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_supplies_in_holidays(holiday_hash)
   i = 0 
   while season_array.length < i do
     puts "#{season_array[i].capitalize!}: #{holiday_array[i].capitalize!}: #{supply_array[i].to_s.capitalize!}" 
-    
+  end   
 end
   
 
