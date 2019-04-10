6b3090cd5e01bd32d34614d3df131cfc42c1a5c8
Henry Koehler <henryk1229@gmail.com>
2019-04-10 17:47:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each 
+    holiday.each do |holiday, supplies|
+      puts ""
 
 end
   
