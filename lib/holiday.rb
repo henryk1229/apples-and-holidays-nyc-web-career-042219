0c409a1546178acc7ac91b83f8663b06f76b6c35
Henry Koehler <henryk1229@gmail.com>
2019-04-10 16:09:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,6 +88,9 @@ def all_supplies_in_holidays(holiday_hash)
   puts "#{season_array[i]}: #{holiday_hash[i]}"
 
 end
+end
+
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
